d198f473a0285460370a84d2bcbe658100d91b44
Vishant <44126013+vishantbhole@users.noreply.github.com>
2025-08-12 22:01:56 -0500

    Update lengthOfLongestSubstring.py

--- lengthOfLongestSubstring.py
+++ lengthOfLongestSubstring.py
@@ -1,2 +1,8 @@
 #3. Longest Substring Without Repeating Characters
 
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
